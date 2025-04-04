25375998c4cbb40c100052c6cbe4635b7a9dea9a
Kristine Karklina <kristine.karklina11@gmail.com>
2025-04-04 09:06:26 +0300

    Changed background and title on GO button click

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+const BODY_CHANGED_CLASSNAME = "body_changed";
+const HEADING_CHANGED = "Hooray! Not bored anymore! 🔥";
+
 const activityNode = document.querySelector(".js-activity");
 const goBtnNode = document.querySelector(".js-btn");
+const bodyNode = document.querySelector("body");
+const titleNode = document.querySelector(".js-title");
 
 goBtnNode.addEventListener("click", function() {
     fetchActivity();
+    changeScreen();
 });
 
 //  Запрос на сервер
-
 function fetchActivity() {
     fetch("https://apis.scrimba.com/bored/api/activity")
         .then(response => response.json())
@@ -15,4 +20,10 @@ function fetchActivity() {
             console.log(data);
             activityNode.innerText = data.activity;
         })
+}
+
+// Изменение экрана
+function changeScreen() {
+    bodyNode.classList.add(BODY_CHANGED_CLASSNAME);
+    titleNode.innerText = HEADING_CHANGED;
 }
